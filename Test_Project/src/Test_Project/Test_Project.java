48b1e61627105a19568bead755b3d63182c20b7b
prize <prize@10.110.209.248>
2024-09-02 16:18:55 -0700

    First Commit

---
+++ Test_Project/src/Test_Project/Test_Project.java
@@ -0,0 +1,10 @@
+package Test_Project;
+
+public class Test_Project{
+	public static void main() {
+		
+		System.out.println("Hello World");
+		//System.out.println("My name is Athena.");
+		
+	}
+}
